5e6a5e97160b2b0ccc1e99140af578ea153e2217
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2011-01-17 00:09:19 +0800

    remove redundant call to 'require rails'

--- lib/default_value_for/railtie.rb
+++ lib/default_value_for/railtie.rb
@@ -21,7 +21,6 @@
 # Rails 3 initialization
 module DefaultValueForPlugin
 	if defined? Rails::Railtie
-		require 'rails'
 		class Railtie < Rails::Railtie
 			initializer 'default_value_for.insert_into_active_record' do
 				ActiveSupport.on_load :active_record do
